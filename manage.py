604c8cddb0fab9925280843aefab97b0ba0effc4
jarvis01 <Daniel K. Zhou/CEN/FOXCONN@FOXCONN>
2020-06-20 19:30:47 +0800

    上传

--- manage.py
+++ manage.py
@@ -6,3 +6,13 @@
 # ======================================================
 
 from flask_script import Manager
+from utils.functions import create_app
+from App import *
+
+app = create_app()
+
+manage = Manager(app=app)
+
+
+if __name__ == '__main__':
+    manage.run(host='0.0.0.0', port=5188)
